c2a2e409b362d155aeaf2eef8dfbef3b5b0c561d
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-30 11:02:42 +0600

    Анализ dll файла и по свойствам класса создаю скрипт для СУБД MsSQL

---
+++ ScriptCreator/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ScriptCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assembly assembly = Assembly.LoadFile(@"C:\Users\1\source\repos\TestApp\obj\Release\netcoreapp3.0\TestApp.dll");
+            Assembly assembly = Assembly.LoadFile(Console.ReadLine());
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("CREATE Database ");
+            var types = new Dictionary<string, string>();
+            types.Add("System.String", "nvarchar(max)");
+            types.Add("System.Guid", "uniqueidentifier");
+            types.Add("System.DateTime", "date");
+            types.Add("System.Boolean", "BIT");
+            types.Add("System.Int64", "bigint");
+            types.Add("System.Byte", "byte");
+            types.Add("System.Byte[]", "bynary");
+            types.Add("System.DateTimeOffset", "datetimeoffset");
+            types.Add("System.Decimal", "decimal");
+            types.Add("System.Double", "float");
+            types.Add("System.Int32", "int");
+            types.Add("System.Int16", "smallint");
+
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.Name == "Program")
+                {
+                    continue;
+                }
+                stringBuilder.Append($"\nCREATE Table {type.Name}\n(");
+                foreach (var memberInfo in type.GetMembers())
+                {
+                    if (memberInfo is PropertyInfo)
+                    {
+                        stringBuilder.Append($"\n  {memberInfo.Name} ");
+                        var propertyInfo = memberInfo as PropertyInfo;
+                        if (!propertyInfo.PropertyType.ToString().Contains("Nullable"))
+                        {
+                            if (types[propertyInfo.PropertyType.ToString()].Contains("uniqueidentifier"))
+                            {
+                                stringBuilder.Append("uniqueidentifier primary key, ");
+                            }
+                            else
+                            {
+                                stringBuilder.Append($"{types[propertyInfo.PropertyType.ToString()]} not null,");
+                            }
+                        }
+                        else
+                        {
+                            stringBuilder.Append($"{types[DeleteNullable(propertyInfo.PropertyType.ToString())]}");
+                        }
+                    }
+                }
+                stringBuilder.Append("\n)");
+            }
+            var script = stringBuilder.ToString();
+
+            Console.WriteLine(script);
+
+
+            Console.ReadKey();
+        }
+
+        public static string DeleteNullable(string text)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            var beginIndex = text.LastIndexOf('[');
+            var endIndex = text.LastIndexOf(']') - 1;
+            while (beginIndex < endIndex)
+            {
+                beginIndex++;
+                stringBuilder.Append(text[beginIndex]);
+            }
+            var txt = stringBuilder.ToString();
+            return txt;
+        }
+    }
+}
+
